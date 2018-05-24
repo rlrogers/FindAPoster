725e120445b4cef444ae65d15b666b652b9c9360
RRogers <rrogers@RRogerss-Mac-Pro.local>
2018-05-24 14:34:43 -0500

    updates

--- selection.py
+++ selection.py
@@ -8,44 +8,44 @@ color_tertiary02 = ["Teal", "Violet", "Magenta"]
 posters_to_present = {
     "Primary"{
         "Red"{
-            0: url,
-            1: url,
-            3: url,
-            4: url,
-            5: url,
+            0: "https://www.zazzle.com/amsterdam_holland_vintage_travel_poster-228611273988513096",
+            1: "https://www.zazzle.com/vintage_movie_ticket_poster-228784519045641996",
+            3: "https://www.zazzle.com/vintage_sells_floto_circus_poster-228735653346897937",
+            4: "https://www.zazzle.com/nasa_future_travel_poster_visit_planet_mars-228202043845425717",
+            5: "https://www.zazzle.com/sensual_awakening_fine_art_poster_print-228044822132537086",
             6: url,
             7: url,
             8: url,
             9: url,
             10: url
         } "Yellow"{
-            0: url,
-            1: url,
-            3: url,
-            4: url,
-            5: url,
+            0: "https://www.zazzle.com/praha_vintage_travel_poster-228259909344044717",
+            1: "https://www.zazzle.com/vase_with_twelve_sunflowers_van_gogh_fine_art_poster-228149054281043212",
+            3: "https://www.zazzle.com/vintage_botanical_cactus_poster-228270196794489031",
+            4: "https://www.zazzle.com/botanical_print_of_daffodils_original_by_redoute-228891606174699984",
+            5: "https://www.zazzle.com/a_group_of_daffodils_poster-228797793574432949",
             6: url,
             7: url,
             8: url,
             9: url,
             10: url
         }"Blue"{
-            0: url,
-            1: url,
-            3: url,
-            4: url,
-            5: url,
-            6: url,
-            7: url,
+            0: "https://www.zazzle.com/cactus_plant_watercolor_on_pink_modern_nature_art_poster-228519420290550713",
+            1: "https://www.zazzle.com/two_girls_vintage_fine_art_poster-228207806866024007",
+            3: "https://www.zazzle.com/nasa_future_travel_poster_planet_with_no_star-228756094886372657",
+            4: "https://www.zazzle.com/blue_beach_waves_on_sand_coastal_photo_art_poster-228056022962752689",
+            5: "https://www.zazzle.com/seascape_beach_and_waves_watercolor_poster-228123393566585548",
+            6: "https://www.zazzle.com/blue_watercolor_abstract_3_poster-228397149536908908",
+            7: "https://www.zazzle.com/kansas_city_skyline_poster-228020614684028272",
             8: url,
             9: url,
             10: url
         }
     } "Secondary"{
         "Green"{
-            0: url,
-            1: url,
-            3: url,
+            0: "https://www.zazzle.com/succulent_plant_poster-228856502662564570",
+            1: "https://www.zazzle.com/retro_palm_springs_house_poster-228580492678354688",
+            3: "https://www.zazzle.com/vintage_botanical_print_bracken_ferns-228384581007189592",
             4: url,
             5: url,
             6: url,
@@ -54,7 +54,7 @@ posters_to_present = {
             9: url,
             10: url
         } "Purple"{
-            0: url,
+            0: "https://www.zazzle.com/girly_chevron_pattern_cute_pink_teal_nebula_galaxy_poster-228283813716993051",
             1: url,
             3: url,
             4: url,
@@ -65,8 +65,8 @@ posters_to_present = {
             9: url,
             10: url
         }"Orange"{
-            0: url,
-            1: url,
+            0: "https://www.zazzle.com/prague_the_city_of_art_vintage_travel_poster-228684533669319387",
+            1: "https://www.zazzle.com/teal_tribal_paisley_elephant_purple_henna_pattern_poster-228994407917115512",
             3: url,
             4: url,
             5: url,
@@ -78,30 +78,30 @@ posters_to_present = {
         }
     } "Tertiary 01"{
         "Vermillion"{
-            0: url,
-            1: url,
-            3: url,
-            4: url,
-            5: url,
-            6: url,
-            7: url,
+            0: "https://www.zazzle.com/mars_valles_marineris_space_tourism_illustration_poster-228833344064386293",
+            1: "https://www.zazzle.com/vintage_travel_firenze_italy_print-228811610719424571",
+            3: "https://www.zazzle.com/colorful_chevron_zig_zag_tribal_aztec_ikat_pattern_poster-228378484624987261",
+            4: "https://www.zazzle.com/orange_fine_art_print-228240747476691289",
+            5: "https://www.zazzle.com/matisse_collioure_poster-228815267808350996",
+            6: "https://www.zazzle.com/education_first_poster-228128793450777972",
+            7: "https://www.zazzle.com/chicken_pot_pi_and_i_dont_care_poster-228476296574424889",
             8: url,
             9: url,
             10: url
         } "Amber"{
-            0: url,
-            1: url,
-            3: url,
-            4: url,
-            5: url,
+            0: "https://www.zazzle.com/mars_retro_space_travel_illustration_poster-228537231656320790",
+            1: "https://www.zazzle.com/nasa_future_travel_poster_relax_on_kepler_16b-228227816154710976",
+            3: :"https://www.zazzle.com/the_great_pyramids_of_giza_egypt_poster-228341198595637366",
+            4: "https://www.zazzle.com/a_dulcian_an_oboe_a_bassoon_an_oboe_da_caccia_a_poster-228325302570573182",
+            5: "https://www.zazzle.com/periodic_table_elemental_gold_fish_poster-228963951842935841",
             6: url,
             7: url,
             8: url,
             9: url,
             10: url
         }"Chartreuse"{
-            0: url,
-            1: url,
+            0: "https://www.zazzle.com/nasa_exoplanet_retro_earth_tour_travel_poster-228927299341904567",
+            1: "https://www.zazzle.com/ikat_andes_aztec_retro_teal_green_chevron_pattern_poster-228437606914715935",
             3: url,
             4: url,
             5: url,
@@ -113,18 +113,18 @@ posters_to_present = {
         }
     } "Tertiary 02"{
         "Teal"{
-            0: url,
-            1: url,
-            3: url,
-            4: url,
-            5: url,
-            6: url,
-            7: url,
+            0: "https://www.zazzle.com/watercolor_succulent_green_cactus_plant_poster-228767555166722819",
+            1: "https://www.zazzle.com/london_vintage_travel_poster-228596044588109358",
+            3: "https://www.zazzle.com/ocean_liner_vintage_travel_poster-228453597140979552",
+            4: "https://www.zazzle.com/two_oil_cans_poster-228797260396543290",
+            5: "https://www.zazzle.com/blue_white_watercolor_brushstrokes_ocean_waves_poster-228466561717559923",
+            6: "https://www.zazzle.com/april_turquoise_and_grey_abstract_art_poster-228712952381772271",
+            7: "https://www.zazzle.com/van_gogh_blossoming_almond_tree_fine_art_poster-228151353917602103",
             8: url,
             9: url,
             10: url
         } "Violet"{
-            0: url,
+            0: "https://www.zazzle.com/eiffel_tower_poster-228641040779427560",
             1: url,
             3: url,
             4: url,
@@ -135,8 +135,8 @@ posters_to_present = {
             9: url,
             10: url
         }"Magenta"{
-            0: url,
-            1: url,
+            0: "https://www.zazzle.com/sugar_coded_pink_abstract_art_poster_print-228589031082563954",
+            1: "https://www.zazzle.com/sunset_behind_seattle_skyline_from_kerry_park_poster-228488875435299973",
             3: url,
             4: url,
             5: url,
