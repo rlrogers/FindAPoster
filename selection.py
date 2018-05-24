4c04478b2a4db64cc556fc71a1c6f622ff5bc9ee
RRogers <rrogers@RRogerss-Mac-Pro.local>
2018-05-24 13:48:38 -0500

    selection bare bones

---
+++ selection.py
@@ -0,0 +1,150 @@
+#Selection of posters
+color_primary = ["Red", "Yellow", "Blue"]
+color_secondary = ["Green", "Purple", "Orange"]
+color_tertiary01 = ["Vermillion", "Amber", "Chartreuse"]
+color_tertiary02 = ["Teal", "Violet", "Magenta"]
+
+
+posters_to_present = {
+    "Primary"{
+        "Red"{
+            0: url,
+            1: url,
+            3: url,
+            4: url,
+            5: url,
+            6: url,
+            7: url,
+            8: url,
+            9: url,
+            10: url
+        } "Yellow"{
+            0: url,
+            1: url,
+            3: url,
+            4: url,
+            5: url,
+            6: url,
+            7: url,
+            8: url,
+            9: url,
+            10: url
+        }"Blue"{
+            0: url,
+            1: url,
+            3: url,
+            4: url,
+            5: url,
+            6: url,
+            7: url,
+            8: url,
+            9: url,
+            10: url
+        }
+    } "Secondary"{
+        "Green"{
+            0: url,
+            1: url,
+            3: url,
+            4: url,
+            5: url,
+            6: url,
+            7: url,
+            8: url,
+            9: url,
+            10: url
+        } "Purple"{
+            0: url,
+            1: url,
+            3: url,
+            4: url,
+            5: url,
+            6: url,
+            7: url,
+            8: url,
+            9: url,
+            10: url
+        }"Orange"{
+            0: url,
+            1: url,
+            3: url,
+            4: url,
+            5: url,
+            6: url,
+            7: url,
+            8: url,
+            9: url,
+            10: url
+        }
+    } "Tertiary 01"{
+        "Vermillion"{
+            0: url,
+            1: url,
+            3: url,
+            4: url,
+            5: url,
+            6: url,
+            7: url,
+            8: url,
+            9: url,
+            10: url
+        } "Amber"{
+            0: url,
+            1: url,
+            3: url,
+            4: url,
+            5: url,
+            6: url,
+            7: url,
+            8: url,
+            9: url,
+            10: url
+        }"Chartreuse"{
+            0: url,
+            1: url,
+            3: url,
+            4: url,
+            5: url,
+            6: url,
+            7: url,
+            8: url,
+            9: url,
+            10: url
+        }
+    } "Tertiary 02"{
+        "Teal"{
+            0: url,
+            1: url,
+            3: url,
+            4: url,
+            5: url,
+            6: url,
+            7: url,
+            8: url,
+            9: url,
+            10: url
+        } "Violet"{
+            0: url,
+            1: url,
+            3: url,
+            4: url,
+            5: url,
+            6: url,
+            7: url,
+            8: url,
+            9: url,
+            10: url
+        }"Magenta"{
+            0: url,
+            1: url,
+            3: url,
+            4: url,
+            5: url,
+            6: url,
+            7: url,
+            8: url,
+            9: url,
+            10: url
+        }
+    }
+}
