6baf75c713727a24dcbacc564240ab2f9c80884e
Jordan Winfery <jwinf843@gmail.com>
2018-05-30 23:37:17 +0000

    imported and refactored code

---
+++ constant.py
@@ -0,0 +1,20 @@
+COLOR_RED = "red"
+COLOR_YELLOW = "yellow"
+COLOR_BLUE = "blue"
+COLOR_GREEN = "green"
+COLOR_PURPLE = "purple"
+COLOR_ORANGE = "orange"
+COLOR_VERMILION = "vermilion"
+COLOR_AMBER = "amber"
+COLOR_CHARTREUSE = "chartreuse"
+COLOR_TEAL = "teal"
+COLOR_VIOLET = "violet"
+COLOR_MAGENTA = "magenta"
+
+PRIMARY = []
+SECONDARY = []
+TERTIARY_01 = []
+TERTIARY_02 = []
+
+MATCH = "match"
+COMPLEMENT = "complement"
