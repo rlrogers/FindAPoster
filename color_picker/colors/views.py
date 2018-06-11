7860c6191b0b1b9c08f7b8472cb4a36c23fed280
Jordan Winfery <jwinf843@gmail.com>
2018-06-11 09:25:53 +0000

    Modified views so that POST request data is saved in a dictionary and list for later use in generating the poster-page.html render

--- color_picker/colors/views.py
+++ color_picker/colors/views.py
@@ -7,5 +7,28 @@ def index(request):
     if request.method == 'GET':
         return render(request, 'index.html')
         
+    #Post Request Data
+    color = {
+        'red':request.POST['red'],
+        'yellow':request.POST['yellow'],
+        'blue':request.POST['blue'],
+        'green':request.POST['green'],
+        'purple':request.POST['purple'],
+        'orange':request.POST['orange'],
+        'vermilion':request.POST['vermilion'],
+        'amber':request.POST['amber'],
+        'chartreuse':request.POST['chartreuse'],
+        'teal':request.POST['teal'],
+        'violet':request.POST['violet'],
+        'magenta':request.POST['magenta'],
+    }
+    
+    #Dynamic
+    color = []
+    
+    for key, value in request.POST:
+        if value == True:
+            color.append(key)
+        
     return render(request, 'poster-page.html')
     
