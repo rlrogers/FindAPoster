4c4d8d98221539713dea9a58152e984d24b2eb3f
Richard L Rogers <richard+c9@richardlrogers.com>
2018-06-13 20:18:44 +0000

    test file

---
+++ color_picker/tests.py
@@ -0,0 +1,123 @@
+# from django.test import TestCase
+from selection import *
+from constant import *
+from main import *
+#______________________
+# Create your tests here.
+#matching testing
+assert(get_matching_poster("red"))
+assert(get_matching_poster("blue"))
+assert(get_matching_poster("yellow"))
+assert(get_matching_poster("green"))
+assert(get_matching_poster("purple"))
+assert(get_matching_poster("orange"))
+assert(get_matching_poster("vermilion"))
+assert(get_matching_poster("amber"))
+assert(get_matching_poster("chartreuse"))
+assert(get_matching_poster("teal"))
+assert(get_matching_poster("violet"))
+assert(get_matching_poster("magenta"))
+#complementing testing
+assert(get_complementing_poster("red"))
+assert(get_complementing_poster("red"))
+assert(get_complementing_poster("blue"))
+assert(get_complementing_poster("yellow"))
+assert(get_complementing_poster("green"))
+assert(get_complementing_poster("purple"))
+assert(get_complementing_poster("orange"))
+assert(get_complementing_poster("vermilion"))
+assert(get_complementing_poster("amber"))
+assert(get_complementing_poster("chartreuse"))
+assert(get_complementing_poster("teal"))
+assert(get_complementing_poster("violet"))
+assert(get_complementing_poster("magenta"))
+
+
+print("OHHHH YEAH! -Koolaid Man")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#Primitive testing
+
+# print(get_matching_poster("red"))
+# print(get_matching_poster("blue"))
+# print(get_matching_poster("yellow"))
+# print(get_matching_poster("green"))
+# print(get_matching_poster("purple"))
+# print(get_matching_poster("orange"))
+# print(get_matching_poster("vermilion"))
+# print(get_matching_poster("amber"))
+# print(get_matching_poster("chartreuse"))
+# print(get_matching_poster("teal"))
+# print(get_matching_poster("violet"))
+# print(get_matching_poster("magenta"))
+
+
+
+# print(get_complementing_poster("red"))
+# print(get_complementing_poster("red"))
+# print(get_complementing_poster("blue"))
+# print(get_complementing_poster("yellow"))
+# print(get_complementing_poster("green"))
+# print(get_complementing_poster("purple"))
+# print(get_complementing_poster("orange"))
+# print(get_complementing_poster("vermilion"))
+# print(get_complementing_poster("amber"))
+# print(get_complementing_poster("chartreuse"))
+# print(get_complementing_poster("teal"))
+# print(get_complementing_poster("violet"))
+# print(get_complementing_poster("magenta"))
+
